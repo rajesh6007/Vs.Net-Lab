263a052f58672b58ee48a34cc950f61cf0c114ad
Rajesh Hamal <hamalr551@gmail.com>
2025-03-16 11:34:12 +0545

    This is from master

---
+++ lab1.py
@@ -0,0 +1,7 @@
+def add(num1, num2):
+    print("_____________________________")
+    print("_____________________________")
+    return num1 + num2
+
+print(f"The addition of 10 and 20 :",add(10,20))
+print("Form master")
